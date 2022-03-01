d908536092055678472b0e785ddbfde5e1966375
skewbmaster <35039141+skewbmaster@users.noreply.github.com>
2022-03-01 00:34:27 +0000

    Added somewhat working submit run over a new POST function

--- srcomapi/srcomapi.py
+++ srcomapi/srcomapi.py
@@ -1,3 +1,4 @@
+from urllib import response
 import requests
 from http.client import responses
 from os.path import dirname
@@ -52,6 +53,29 @@ class SpeedrunCom(object):
             data = response.json()["data"]
         return data
 
+    def post(self, endpoint, data, **kwargs):
+        headers = {"User-Agent": self.user_agent}
+        if self.api_key:
+            headers["X-API-Key"] = self.api_key
+        kwargs.update({"headers": headers})
+        uri = API_URL + endpoint
+        if self.debug >= 1: print(uri)
+
+        if self.mock:
+            # Implement if needed
+            data = None
+
+        else:
+            response = requests.post(uri, data=data **kwargs)
+            if response.status_code != 201:
+                raise APIRequestException((response.status_code, responses[response.status_code], uri[len(API_URL):]), response)
+
+            data = response.json()["data"]
+
+        return data
+
+
+
     def get_game(self, id, **kwargs):
         return datatypes.Game(self, data=self.get("games/" + id))
 
@@ -66,3 +90,70 @@ class SpeedrunCom(object):
         """Returns a generator that uses the given datatype and search params to get results"""
         response = self.get(datatype.endpoint, params=params)
         return [datatype(self, data=data) for data in response]
+
+    def submit_run(self, times_obj, variables_obj, players_array, category, level = None, run_date = None, region = None, platform = None, video_link = None, comment = None, splitsio = None, emulated = False, verified = False):
+        """Sends a POST HTTP request with parameters given to this function, """
+        if not self.api_key:
+            return "No API Key Provided"
+
+        rundata = { "run" : {} }
+
+        rundata["run"]["category"] = category.id
+        
+        rundata["run"]["times"] = times_obj
+        rundata["run"]["variables"] = variables_obj
+
+        if players_array != None:
+            rundata["run"]["players"] = players_array
+
+        if level:
+            rundata["run"]["level"] = level.id
+
+        if region:
+            rundata["run"]["region"] = region.id
+
+        if platform:
+            rundata["run"]["platform"] = platform.id
+
+        if run_date:
+            rundata["run"]["date"] = run_date
+
+        if video_link:
+            rundata["run"]["video"] = video_link
+
+        if comment:
+            rundata["run"]["comment"] = comment
+
+        if splitsio:
+            rundata["run"]["splitsio"] = splitsio
+
+        if emulated:
+            rundata["run"]["emulated"] = True
+
+        if verified:
+            rundata["run"]["verified"] = True
+
+        response = self.post(datatypes.Run.endpoint, rundata)
+
+
+    def create_players_obj(self, *players):
+        players_obj = []
+
+        for i, player in enumerate(players):
+            players_obj[i] = {
+                "rel" : player.role,
+                "id" : player.id
+            }
+
+        return players_obj
+
+    def create_variables_obj(self, *variable_tuples):
+        variables_obj = {}
+
+        for vartup in variable_tuples:
+            variables_obj[vartup[0].id] = {
+                "type" : "user_defined" if vartup[0].user_defined else "pre-defined",
+                "value" : vartup[1]
+            }
+        
+        return variables_obj
